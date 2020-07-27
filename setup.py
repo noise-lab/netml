655d8bcd9679b76c4b7ed1eb46487930377ef3d8
Jesse London <jesselondon@gmail.com>
2020-07-27 17:37:10 -0500

    corrected installation instructions and cleaned up setup.py & readme.md
    == Installation
    
    * installation of this library was unclear --
    ** both setup.py and requirements.txt provided, and unconnected to each
    other (latter removed)
    ** unused/unnecessary requirements (commented-out/removed respectively)
    
    * instruction to build & install appears incorrect --
    ** generally unnecessary to `setup.py build` & `setup.py install`; the
    latter command does both
    ** these instructions insufficient, due to scikit-learn -- setuptools
    alone *cannot* install this dependency in this manner, and so this
    procedure fails. (rather this dependency, and therefore the entire
    library, must be installed via `pip` -- as it would be installed from
    PyPI.)
    
    * python version requirement loosened (no need to require a micro
    version -- rather, should aim to support the remainder of the major
    version 3)
    
    * library likely can't yet be considered "production/stable" (rather,
    pre-alpha)
    
    * software license as yet undetermined (but likely not Python
    Foundation's own)
    
    == Documentation
    
    readme.md cleaned up for clarity of language and formatting, and to
    correct installation instructions.

--- setup.py
+++ setup.py
@@ -1,66 +1,58 @@
-"""Setup.py
-    python3 setup.py build
-    python3 setup.py install
+"""setup.py
+
+Due to scikit-learn, pip is required:
+
+    pip install .
+
 """
-# Authors: kun.bj@outlook.com
-#
-# License: xxx
-import os
+import pathlib
 
 from setuptools import find_packages
 from setuptools import setup
 
 
-def read(readme_file):
-    """Utility function to read the README file. Used for the long_description.
+README_PATH = pathlib.Path(__file__).parent / 'readme.md'
 
-    Parameters
-    ----------
-    readme_file: str
+INSTALL_REQUIRES = (
+    'numpy==1.18.3',
+    'pandas==0.25.1',
+    'scapy==2.4.3',
+    'scikit-learn==0.23.1',
+)
 
-    Returns
-    -------
-
-    """
-
-    value = open(os.path.join(os.path.dirname(__file__), readme_file), 'r', encoding='utf-8').read()
-    return str(value)
+EXTRAS_REQUIRE = {
+    # (as yet) unused:
+    # 'visualize': ['matplotlib==3.2.1'],
+    # 'tests': ['pytest==5.3.1', 'requests==2.22.0'],
+}
 
 
 setup(name='odet',
       version='0.0.1',
       description='Novelty Detection',
-      long_description=read('readme.md'),
-      # long_description='Data representation for IoT traffic',
+      long_description=README_PATH.read_text(),
       long_description_content_type="text/markdown",
       author='Kun',
       author_email='kun.bj@outlook.com',
       url='https://github.com/Learn-Live/odet',
       download_url='https://github.com/Learn-Live/odet',
       license='xxx',
-      python_requires='==3.7.3',
-      install_requires=['numpy==1.18.3',
-                        'scipy==1.4.1',
-                        'pandas==0.25.1',
-                        'scapy==2.4.3',
-                        'scikit-learn==0.21.3'
-                        ],
-      extras_require={
-          'visualize': ['matplotlib==3.2.1'],
-          'tests': ['pytest==5.3.1',
-                    'requests==2.22.0'
-                    ],
-      },
+      python_requires='>=3.7.3,<4',
+      install_requires=INSTALL_REQUIRES,
+      extras_require=EXTRAS_REQUIRE,
       classifiers=[
-          'Development Status :: 5 - Production/Stable',
+          'Development Status :: 2 - Pre-Alpha',
           'Intended Audience :: Developers',
           'Intended Audience :: Education',
           'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: Python Software Foundation License',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
           'Topic :: Software Development :: Libraries',
           'Topic :: Software Development :: Libraries :: Python Modules'
+          'Topic :: Scientific/Engineering :: Artificial Intelligence',
+          'Topic :: System :: Networking :: Monitoring',
       ],
       # automatically find the packages with __init__.py file and start from the setup.py's directory
-      packages=find_packages())
+      packages=find_packages(),
+)
