af63074c38ce49ec47c28a4c5c44d5e85b4eb07f
Jesse London <jesselondon@gmail.com>
2020-09-23 17:07:33 -0500

    Bump version: 0.0.3 → 0.1.0

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ EXTRAS_REQUIRE = {
 
 
 setup(name='netml',
-      version='0.0.3',
+      version='0.1.0',
       description='Network anomaly detection via machine learning',
       long_description=README_PATH.read_text(),
       long_description_content_type="text/markdown",
